ff0ef545a9ae3be8e800460db48573e63ba394ea
DolevArtzi <dartzi@andrew.cmu.edu>
2022-06-18 16:57:13 -0400

    remove 'name' from state, add type support for union DFAs

--- finite-automata/dfa/dfa.ts
+++ finite-automata/dfa/dfa.ts
@@ -1,19 +1,16 @@
 import { SetOp } from './setHelper';
-type internal1 = (number | string)
-type internal = internal1 | [internal1,internal1]
-export type State = {
-    name : internal | [internal,internal] | [internal,internal,internal,internal]
-}
-
+export type State = string | number | State[];
 export type Alphabet = Set<string>;
 export type Transition = [State,string,State];
-export type Delta = Set<Transition> //| ((State,string) => State);
+type Rule = Set<Transition>;
+export type TransitionF = (q : State,c : string) => State;
 type arrSet<T> = Array<T> | Set<T>;
 const break_string = '_________________________________________________________________________________________';
 export class DFA {
     Q;
     S;
-    d : Delta;
+    rules : Rule;
+    protected transition_func : (q : State,c : string) => State;
     q0;
     F;
     name;
@@ -23,18 +20,18 @@ export class DFA {
         console.log(`DFA: ${this.name}`);
         console.log(`Q: ${this.statesString(this.Q)}`);
         console.log(`Alphabet: ${this.alphabetString(this.S)}`);
-        console.log(`Transition Function: ${this.deltaString(this.d)}`);
+        console.log(`Transition Function: ${this.deltaString(this.rules)}`);
         console.log(`Starting State: ${this.stateString(this.q0)}`);
         console.log(`Accepting States: ${this.statesString(this.F)}`);
         console.log(break_string);
     }
 
-    private deltaString(d : Delta) {
-        return this.printArrSet(d,this.transitionString);
+    private deltaString(rules : Rule) {
+        return this.printArrSet(rules,this.transitionString);
     }
 
     private stateString(q : State) : string {
-        return "s" + q.name;
+        return "s" + `${q}`;
     }
 
     private printArrSet(set : arrSet<any>, f : (arg0 : any) => string) {
@@ -55,7 +52,7 @@ export class DFA {
     }
 
     private transitionString(t : Transition) {
-        return "{(s" + t[0].name + ", '" + t[1] + "') --> " + " s" + t[2].name + "}";
+        return "{(s" + `${t[0]}` + ", '" + t[1] + "') --> " + " s" + `${t[2]}` + "}";
     }
 
     validateStates(states : State[], Q? : Set<State>) {
@@ -94,8 +91,8 @@ export class DFA {
         }
     }
 
-    validateDelta(d : Delta) {
-        let transitionArr = d.values();
+    validateRule(rules : Rule) {
+        let transitionArr = rules.values();
         for (const t of transitionArr) {
             if (!this.validateTransition(t)) return false;
         }
@@ -104,11 +101,11 @@ export class DFA {
 
     complement() : DFA {
         return new DFA(
-            this.Q,this.S,this.d,this.q0,SetOp.difference(this.Q,this.F),`complement of DFA: ${this.name}`
+            this.Q,this.S,this.rules,this.q0,SetOp.difference(this.Q,this.F),`complement of DFA: ${this}`
         );
     }
 
-    //assumes S1 = S2
+    // assumes S1 = S2
     // union(D2 : DFA) {
     //     let q0z = D2.q0;
     //     let Qz = D2.Q;
@@ -116,30 +113,33 @@ export class DFA {
     //     let namez = D2.name;
 
     //     let Q_n = SetOp.cartesianProduct(this.Q,Qz);
-    //     let d_n = ([q,q_],w) => [this.stepState(w,q),D2.stepState(w,q_)];
+    //     let transition_func_n = ([q,q_],w) => [this.transition_func(q,w),D2.transition_func(q_,w)];
     //     let q0_n = [this.q0,q0z];
     //     let F_n = SetOp.union(SetOp.cartesianProduct(this.Q,Fz),SetOp.cartesianProduct(this.F,Qz));
+    //     let new_transition_func = 
+
     //     return new DFA(
-    //         Q_n,this.S,
-    //     )
+    //         Q_n,this.S,null,q0_n,F_n,`union of ${this.name} and ${D2.name}`
+    //     );
     // }
     
     addRule(t : Transition) {
-        if (this.validateTransition(t)) this.d.add(t);
+        if (this.validateTransition(t)) this.rules.add(t);
     }
 
-    constructor(Q : Set<State>, S : Alphabet, d : Delta, q0 : State, F : Set<State>, name? : string) {
+    constructor(Q : Set<State>, S : Alphabet, rules : Rule, q0 : State, F : Set<State>, name? : string) {
         this.Q = Q;
         this.S = S;
-        this.validate(Q,S,d,q0,F);
-        this.d = d;
+        this.validate(Q,S,rules,q0,F);
+        this.rules = rules;
+        this.transition_func = this.delta;
         this.q0 = q0;
         this.F = F;
         this.name = name;
         this.print();
     }
 
-    validate(Q: Set<State>, S: Alphabet, d: Delta, q0: State, F: Set<State>) {
+    validate(Q: Set<State>, S: Alphabet, rules: Rule, q0: State, F: Set<State>) {
         const invalid = (message? : string) => { throw new TypeError(message); };
 
         F.forEach(state => {
@@ -152,7 +152,7 @@ export class DFA {
             invalid('starting state not in Q');
         }
 
-        if (!this.validateDelta(d)) {
+        if (!this.validateRule(rules)) {
             invalid('invalid transition function');
         }
     }
@@ -165,17 +165,18 @@ export class DFA {
      * @throws {TypeError} if there is no rule to transition @param c from 
      *                     @param curr
      */
-    protected stepState(c : string, curr : State) : State {
+    protected delta(curr : State, c : string) : State {
         let newState = undefined;
-        let transitionArr = this.d.values();
+        let transitionArr = this.rules.values();
 
         for (const rule of transitionArr) {
             const q1 = rule[0];
             const c2 = rule[1];
-            if (c === c2 && curr.name === q1.name) newState = rule[2]; 
+            if (c === c2 && curr === q1) newState = rule[2]; 
         }
 
         if (newState === undefined) throw new TypeError('undefined transition');
+        // if (newState !== undefined) console.log("NOT NULL");
         return newState;
     }
 
@@ -190,7 +191,7 @@ export class DFA {
     private _eval(w : string, curr : State) : boolean {
         if (w === "") return this.F.has(curr);
 
-        const newState = this.stepState(w[0],curr);
+        const newState = this.transition_func(curr,w[0]);
         return this._eval(w.substring(1),newState);
     }
 
--- finite-automata/dfa/exampleDFA.ts
+++ finite-automata/dfa/exampleDFA.ts
@@ -1,19 +1,19 @@
-import { DFA,State,Delta,Alphabet,Transition } from './dfa';
+import { DFA,State,Alphabet,Transition } from './dfa';
 import { SetOp } from './setHelper';
 
 const language_desc = "strings that end with a 0 and empty string";
 
 //set of states
 let Q = new Set<State>();
-let q0 = {name:0}; //initial state
-let q1 = {name:1};
+let q0 = 0; //initial state
+let q1 = 1;
 Q.add(q0).add(q1);
 
 let f = new Set<State>() //set of accepting states
 f.add(q0);
 
 //set of rules, represents the transition function
-let delta : Delta = new Set<Transition>();
+let delta = new Set<Transition>();
 let r1 : Transition = [q0,'0',q0];
 let r2 : Transition = [q0,'1',q1];
 let r3 : Transition = [q1,'1',q1];
@@ -27,7 +27,7 @@ S.add('0');
 
 let d = new DFA(Q, S,delta,q0,f,language_desc);
 let complement = d.complement();
-d.addRule([q0,'1',{name:3}]); //invalid rule
+d.addRule([q0,'1',3]); //invalid rule
 // d.eval('010101110'); //1
 // d.eval('01010111'); //0
 // d.eval(''); //1
@@ -42,4 +42,4 @@ for(let i = 0; i < 5; i++) {
 
 let s1 = new Set([1,2,3,4,5,6,7,8,9,10]);
 let s2 = new Set(['a','b','c','d','e','f','g','h']);
-let cp = SetOp.cartesianProduct(s1,s2);
+// let cp = SetOp.cartesianProduct(s1,s2);
